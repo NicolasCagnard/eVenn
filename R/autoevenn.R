b206bc02c058350934bedb31209b1229de381e4c
NicolasCagnard <nicolas.cagnard@gmail.com>
2015-10-07 15:45:12 +0200

    new file:   DESCRIPTION 	new file:   NAMESPACE 	new file:   R/autoevenn.R 	new file:   R/evenn.R 	new file:   R/man.evenn.R 	new file:   data/Data_Binary_Matrix.rda 	new file:   data/Data_Lists.rda 	new file:   data/Data_Matrix_Of_Folds.rda 	new file:   data/Data_Matrix_Of_Ratios.rda 	new file:   man/Data_Binary_Matrix.Rd 	new file:   man/Data_Lists.Rd 	new file:   man/Data_Matrix_Of_Folds.Rd 	new file:   man/Data_Matrix_Of_Ratios.Rd 	new file:   man/autoevenn.Rd 	new file:   man/eVenn-package.Rd 	new file:   man/evenn.Rd 	new file:   man/man.evenn.Rd

---
+++ R/autoevenn.R
@@ -0,0 +1,25 @@
+autoevenn <-function(FolderPath="", annot=TRUE, ud=TRUE, VennBar=FALSE, Solid=TRUE, Profils=TRUE, prop=FALSE, display=FALSE, Ptest=FALSE)
+{
+	if(FolderPath=="")
+	{
+		write(paste("FolderPath is empty, it must contain the directory where are placed the Folders > Lists" , sep=""), file="")
+		flush.console()
+		FolderPath = choose.dir() 
+		break
+	}
+	
+	Venns = list.dirs(FolderPath, full.names=TRUE)
+	
+	FolderDest = paste(getwd(), "/Venn.diagrams/", sep="")
+	dir.create(file.path(FolderDest), showWarnings = FALSE)
+	FolderDest = paste(getwd(), "/Venn.diagrams/", basename(FolderPath), sep="")
+	dir.create(file.path(FolderDest), showWarnings = FALSE)
+	
+	Venns = Venns[Venns!=FolderPath]  # filtre le repertoire principal de la liste
+	write(paste(length(Venns), " Venn", if(length(Venns)>1){paste("s", sep="")}, sep=""), file="")
+	for(V in 1:length(Venns))
+	{
+		write(paste(V, " / ", length(Venns), ": ", basename(Venns[V]), sep=""), file="")
+		evenn(annot=annot, ud=ud, VennBar=VennBar, pathRes=FolderDest, pathLists=Venns[V], display=display, Solid=Solid, Profils=Profils, prop=prop, Ptest=Ptest)
+	}
+}
